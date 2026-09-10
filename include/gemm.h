e72da10bf0faa8e392adf80f7374a792a53ed498
BubblerLIU <lium_ex2020@163.com>
2026-09-10 01:28:51 +0000

    tiled framework

--- include/gemm.h
+++ include/gemm.h
@@ -3,3 +3,7 @@
 void launch_gemm_naive(
     const float *A, const float *B, float *C, int M, int N, int K
 );
+
+void launch_gemm_tiled(
+    const float *A, const float *B, float *C, int M, int N, int K
+)
---
+++ src/tiled_gemm.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+
+constexpr TILE = 16;
+
+__global__ void gemm_tiled(
+    const float *A, const float *B, float *C, int M, int N, int K
+) {
+    __shared__ float As[TILE][TILE];
+    __shared__ float Bs[TILE][TILE];
+
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    
+}
+
+void launch_gemm_tiled(
+    const float *A, const float *B, float *C, int M, int N, int K
+) {
+
+}
